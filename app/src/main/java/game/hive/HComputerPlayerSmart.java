2349615cd6615666e207d6f293f286cbaadbba2f
romerohe23 <romerohe23@up.edu>
2025-10-23 14:20:34 -0700

    Created ComputerPlayerSmart Class

---
+++ app/src/main/java/game/hive/HComputerPlayerSmart.java
@@ -0,0 +1,31 @@
+package game.hive;
+
+import game.hive.GameFramework.infoMessage.GameInfo;
+import game.hive.GameFramework.players.GameComputerPlayer;
+
+public class HComputerPlayerSmart extends GameComputerPlayer {
+
+    /*
+     * Constructor for the TTTComputerPlayer1 class
+     */
+    public HComputerPlayerSmart(String name) {
+        // invoke superclass constructor
+        super(name); // invoke superclass constructor
+    }
+    /**
+     * Called when the player receives a game-state (or other info) from the
+     * game.
+     *
+     * @param info
+     * 		the message from the game
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+
+
+        //Logger.log("TTTComputer", "Sending move");
+        //game.sendAction(new TTTMoveAction(this, yVal, xVal));
+
+    }
+
+}
