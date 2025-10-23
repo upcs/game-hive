97983f5f7e5b3efe2a9fc8f5ff87758122485808
romerohe23 <romerohe23@up.edu>
2025-10-23 13:50:26 -0700

    Created and C/P TTT ComputerPlayer Class

---
+++ app/src/main/java/game/hive/HComputerPlayer.java
@@ -0,0 +1,32 @@
+package game.hive;
+
+import game.hive.GameFramework.infoMessage.GameInfo;
+import game.hive.GameFramework.players.GameComputerPlayer;
+import game.hive.GameFramework.utilities.Logger;
+
+public class HComputerPlayer extends GameComputerPlayer {
+
+    /*
+     * Constructor for the TTTComputerPlayer1 class
+     */
+    public HComputerPlayer(String name) {
+        // invoke superclass constructor
+        super(name); // invoke superclass constructor
+    }
+
+    /**
+     * Called when the player receives a game-state (or other info) from the
+     * game.
+     *
+     * @param info
+     * 		the message from the game
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+
+
+        //Logger.log("TTTComputer", "Sending move");
+        //game.sendAction(new TTTMoveAction(this, yVal, xVal));
+
+    }
+}
