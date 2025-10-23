37b419b04167a6d26626dc929ea4965186ded659
ngDaniel025 <nguyenda25@up.edu>
2025-10-23 14:20:14 -0700

    Updated git ignore and added methodsin HHumanPLayer

--- app/src/main/java/game/hive/HHumanPlayer.java
+++ app/src/main/java/game/hive/HHumanPlayer.java
@@ -1,4 +1,24 @@
 package game.hive;
 
-public class HHumanPlayer {
+import android.view.View;
+
+import game.hive.GameFramework.GameMainActivity;
+import game.hive.GameFramework.infoMessage.GameInfo;
+import game.hive.GameFramework.players.GameHumanPlayer;
+
+public class HHumanPlayer extends GameHumanPlayer {
+    @Override
+    public View getTopView() {
+        return null;
+    }
+
+    @Override
+    public void receiveInfo(GameInfo info) {
+
+    }
+
+    @Override
+    public void setAsGui(GameMainActivity activity) {
+
+    }
 }
