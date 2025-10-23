377d52b1f5b26377c9b91e7a057c521e2ba90f6b
Lucaspab <lucaspab28@up.edu>
2025-10-23 13:46:20 -0700

    Added HMainAcivity

---
+++ app/src/main/java/game/hive/HMainActivity.java
@@ -0,0 +1,73 @@
+package game.hive;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+import game.hive.GameFramework.GameMainActivity;
+import game.hive.GameFramework.LocalGame;
+import game.hive.GameFramework.gameConfiguration.GameConfig;
+import game.hive.GameFramework.gameConfiguration.GamePlayerType;
+import game.hive.GameFramework.infoMessage.GameState;
+import game.hive.GameFramework.players.GamePlayer;
+
+public class HMainActivity extends GameMainActivity {
+    private static final String TAG = "TTTMainActivity";
+    public static final int PORT_NUMBER = 5213;
+
+    /**
+     *
+     */
+    @Override
+    public GameConfig createDefaultConfig() {
+        // Define the allowed player types
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+        // yellow-on-blue GUI
+        playerTypes.add(new GamePlayerType("Local Human Player (blue-yellow)") {
+            public GamePlayer createPlayer(String name) {
+                return new HHumanPlayer(name, R.layout.activity_main);
+            }
+        });
+        // dumb computer player
+        playerTypes.add(new GamePlayerType("Computer Player (dumb)") {
+            public GamePlayer createPlayer(String name) {
+                return new HComputerPLayerDumb(name);
+            }
+        });
+        // Create a game configuration class for Tic-tac-toe
+        GameConfig defaultConfig = new GameConfig(playerTypes, 2,2, "Tic-Tac-Toe", PORT_NUMBER);
+
+        // Add the default players
+        defaultConfig.addPlayer("Human", 0); // local human
+        defaultConfig.addPlayer("Computer", 1); // dumb computer player
+
+        // Set the initial information for the remote player
+        defaultConfig.setRemoteData("Remote Player", "", 1);
+
+        //done!
+        return defaultConfig;
+    }
+    /**
+     * createLocalGame
+     *
+     * Creates a new game that runs on the server tablet,
+     * @param gameState
+     * 				the gameState for this game or null for a new game
+     *
+     * @return a new, game-specific instance of a sub-class of the LocalGame
+     *         class.
+     */
+    @Override
+    public LocalGame createLocalGame(GameState gameState){
+        if(gameState == null)
+            return new HLocalGame();
+        return new HLocalGame((HState) gameState);
+    }
+
+}
