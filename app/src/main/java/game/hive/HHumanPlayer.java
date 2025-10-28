97b9a6761047c8e02d0aa35ea29ea239ff3f7805
ethanmagicbrown <158209436+ethanmagicbrown@users.noreply.github.com>
2025-10-28 13:23:27 -0700

    added gui xml and more HHuman player code

--- app/src/main/java/game/hive/HHumanPlayer.java
+++ app/src/main/java/game/hive/HHumanPlayer.java
@@ -7,6 +7,13 @@ import game.hive.GameFramework.infoMessage.GameInfo;
 import game.hive.GameFramework.players.GameHumanPlayer;
 
 public class HHumanPlayer extends GameHumanPlayer {
+
+    
+    private int layoutId;
+    public HHumanPlayer(String name, int layoutId) {
+        super(name);
+        this.layoutId = layoutId;
+    }
     @Override
     public View getTopView() {
         return null;
@@ -19,6 +26,8 @@ public class HHumanPlayer extends GameHumanPlayer {
 
     @Override
     public void setAsGui(GameMainActivity activity) {
-
+        //This is basically the onCreate method
+        //Set contentView here
+        activity.setContentView(layoutId);
     }
 }
