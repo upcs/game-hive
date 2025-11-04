56158dc2217584844c0a7b43611f28a5834554d8
ngDaniel025 <nguyenda25@up.edu>
2025-11-04 14:21:27 -0800

    Update HSurfaceView.java
    Added mapPixeltoHex into surface iew, and made red line draw across the screen

--- app/src/main/java/game/hive/State/HSurfaceView.java
+++ app/src/main/java/game/hive/State/HSurfaceView.java
@@ -4,23 +4,30 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
 import android.view.SurfaceView;
 
 public class HSurfaceView extends SurfaceView {
 
-    public HSurfaceView(Context context) {
-        super(context);
+    public HSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         setWillNotDraw(false);
     }
     @Override
     public void onDraw(Canvas canvas) {
         Paint hexColor = new Paint();
         hexColor.setColor(Color.RED);
-        drawHex(1,2,10,10,hexColor, canvas);
+        drawHex(1,2,500,500,hexColor, canvas);
     }
 
     public void drawHex(float x, float y, float x2, float y2, Paint color, Canvas canvas){
+
         canvas.drawLine(x, y, x2, y2, color);
+    }
+    public Point mapPixelToHex(int x, int y){
+
+        return new Point(0,0);
     }
 
 }
