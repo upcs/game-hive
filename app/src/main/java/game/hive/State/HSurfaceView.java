a961670a5b5526ce0d972c1a2ed8b83eac331935
Lucaspab <lucaspab28@up.edu>
2025-11-06 13:58:34 -0800

    Created helper method for creating image
    created a helper method for the hashmap so it can drawPiece for any piece

--- app/src/main/java/game/hive/State/HSurfaceView.java
+++ app/src/main/java/game/hive/State/HSurfaceView.java
@@ -20,6 +20,8 @@ public class HSurfaceView extends SurfaceView {
     //private Bitmap myImageBitmap;
     private HashMap<String, Bitmap> pieces;
     private final int LENGTH = 40;
+    final int a = (int) (LENGTH * Math.cos(0.523599));
+    final int b = (int) (LENGTH * Math.sin(0.523599));
     
 
     public HSurfaceView(Context context, AttributeSet attrs) {
@@ -42,7 +44,7 @@ public class HSurfaceView extends SurfaceView {
         super.onDraw(canvas);
         if(pieces != null){
             Rect srcRect = null;//new Rect(0,0, pieces.getWith(), pieces.getHeight());
-            Rect dstRect = new Rect(50,50,250,250);
+            Rect dstRect = new Rect(0,0,LENGTH,2*b);
             //Paint paint = new Paint();
             canvas.drawBitmap(pieces.get("Beetle"),srcRect,dstRect,null);
 
@@ -52,10 +54,15 @@ public class HSurfaceView extends SurfaceView {
             canvas.drawBitmap(pieces.get("Beetle"),0,0,null);
         }*/
     }
+    public void drawPiece(Canvas canvas, String namePiece){
+        Rect srcRect = null;//new Rect(0,0, pieces.getWith(), pieces.getHeight());
+        Rect dstRect = new Rect(0,0,LENGTH,2*b);
+        //Paint paint = new Paint();
+        canvas.drawBitmap(pieces.get(namePiece),srcRect,dstRect,null);
+    }
 
     public void drawHex(float x, float y, float x2, float y2, Paint color, Canvas canvas){
-        final int a = (int) (LENGTH * Math.cos(0.523599));
-        final int b = (int) (LENGTH * Math.sin(0.523599));
+
 
         color.setStyle(Paint.Style.STROKE);
         color.setStrokeWidth(5);
