0543d68312bb39d6a973815f220f9780264cc048
Lucaspab <lucaspab28@up.edu>
2025-11-18 13:04:49 -0800

    test case for testFirstPieceForcedToCenter
    makes sure that the first piece that is being placed is placed to the center rather than any other spot just to make sure so it doesn't disrupt with the board

--- app/src/test/java/game/hive/HiveTest.java
+++ app/src/test/java/game/hive/HiveTest.java
@@ -1,8 +1,18 @@
 package game.hive;
 
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import game.hive.State.HGameState;
+import game.hive.State.HexSpace;
 
 @RunWith(RobolectricTestRunner.class)
 public class HiveTest {
@@ -13,5 +23,17 @@ public class HiveTest {
         activity = Robolectric.buildActivity(HMainActivity.class).create().resume().get();
     }
     @Test
+    public void testFirstPieceForcedToCenter() {
+        HGameState state = new HGameState();
 
+        boolean result = state.placePiece(0, 0, "Ant", state.getActivePlayer());
+        assertTrue(result);
+
+        HexSpace hex = state.getBoard().get(15).get(15);
+        assertNotNull(hex.getHex());
+        assertEquals("Ant", hex.getHex().getName());
+
+        // Check the board did NOT place at the passed location
+        assertNull(state.getBoard().get(0).get(0).getHex());
+    }
 }
